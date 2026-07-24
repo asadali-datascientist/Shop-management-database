e4b92cb750c74ded847b67f5420077de860ecd14
Asad <Asad@gmail.com>
2026-07-24 18:47:42 +0500

    update db

--- db.py
+++ db.py
@@ -3,6 +3,7 @@ import streamlit as st
 
 conn = mysql.connector.connect(
     host=st.secrets["DB_HOST"],
+    port=int(st.secrets["DB_PORT"]),
     user=st.secrets["DB_USER"],
     password=st.secrets["DB_PASSWORD"],
     database=st.secrets["DB_NAME"]
